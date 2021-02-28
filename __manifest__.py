b95b12b2b5d2f184aed5922a46a0bc2f913106ce
Haris Sheikh <razaharis132@gmail.com>
2021-02-28 23:52:27 +0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name':'School Management',
+    'version':'13.0.1.0.0',
+    'category':'Extra Tools',
+    'summary':'Module for Hospital management',
+    'sequence':'10',
+    'license':'AGPL-3',
+    'author':'haris',
+    'maintainer':'haris',
+    'website':'haris.com',
+    'depends':['base','mail'],
+    'demo':[],
+    'qweb':[],
+    'data':[
+        'views/student.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable':True,
+    'application':True,
+    'auto_install':False,
+
+}
